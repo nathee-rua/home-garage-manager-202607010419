de226aa9b474e403d3aa26144b9a76ba79aca516
nathee-rua <nathee-rua@users.noreply.github.com>
2026-07-03 00:10:38 +0700

    style: add global alternating row highlight (zebra striping) using warm muted theme color for all tables

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -33,7 +33,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-colors even:bg-muted/30 hover:bg-muted/60 data-[state=selected]:bg-muted",
       className
     )}
     {...props}
